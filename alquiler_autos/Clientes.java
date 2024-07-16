53dcaed17ebb3d86a6e7ac960506d9b2f6ecab4d
arielcrespin <63946116+arielcrespin@users.noreply.github.com>
2024-07-16 01:32:45 -0300

    Add files via upload

--- alquiler_autos/Clientes.java
+++ alquiler_autos/Clientes.java
@@ -1,1 +1,40 @@
-
+public class Cliente{
+	private String cbu,tarjetadebito,tarjetacredito,patente,vehiculo,modelo,nombre,apellido,direccion,telefono,email,dni;
+	public String obtenerCbu() {
+		return cbu;
+	}
+	public String obtenerPatente() {
+		return patente;
+	}
+	public String obtenerVehiculo() {
+		return vehiculo;
+	}
+	public String obtenerModelo() {
+		return modelo;
+	}
+	public String obtenerNombre(){
+		return nombre;
+	}
+	public String obtenerApellido(){
+		return apellido;
+	}
+	public String obtenerDireccion(){
+		return direccion;
+	}
+	public String obtenerTelefono(){
+		return telefono;
+	}
+	public String obtenerEmail(){
+		return email;
+	}
+	public String obtenerDni(){
+		return dni;
+	}
+	public String obtenerTarjetaDebito(){
+		return tarjetadebito;
+	}
+	public String obtenerTarjetaCredito() {
+		return tarjetacredito;
+	}
+	
+}
